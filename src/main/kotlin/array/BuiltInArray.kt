7419cf32892bc34877fc67279da4483e686df654
Nona Yorn <yornnonakh@gmail.com>
2026-05-21 13:58:55 +0700

    add the new feature of array 21,05,2026 by feature_login branch z

--- src/main/kotlin/array/BuiltInArray.kt
+++ src/main/kotlin/array/BuiltInArray.kt
@@ -1,11 +1,5 @@
 package main.kotlin.array
-
 import javax.imageio.ImageReadParam
-
-/*
-    How to create builtInArray In Kotlin
-    How tousing array in kotlin programing
- */
 
 fun main(){
     /*
@@ -13,15 +7,18 @@ fun main(){
         - Mutable Array => can change the index in array
         - Immutable Array => can't change the index in array
      */
-    println("=====================")
     //    val intArray: List<Int> = listOf<Int>()
     val intArray = listOf<Int>(20)
     val stringArray: Array<String> = arrayOf<String>("String","ArrayOf")
     val studentArray = arrayOf<Int>(99)
     val productStudent = arrayOf<String>("Product Student")
-    val stringArray1: Array<Int> = emptyArray<Int>() // emptyArray in kotlin
-    val emptyList: List<Int> = emptyList() // emptyList equal = [null]
+    val stringArray1: Array<Int> = emptyArray<Int>()
+    // emptyArray in kotlin
+    val emptyList: List<Int> = emptyList()
+    // emptyList equal = [null]
     emptyList.plus(2)
+
+    println(message = "String Array: ${stringArray.contentToString()}")
     println(emptyList)
     intArray.plus("Sting")
     println("--------------------------------")
