46c24d623c395c101271c13b8b2bb1c2bb3d9d5b
Nona Yorn <yornnonakh@gmail.com>
2026-05-21 20:42:10 +0700

    add the new feature

--- src/main/kotlin/typesalise/TypesAlise.kt
+++ src/main/kotlin/typesalise/TypesAlise.kt
@@ -1,5 +1,44 @@
 package main.kotlin.typesalise
 
+import main.kotlin.datatypes.requestNetwork
+import kotlin.Boolean
+import kotlin.String
+
+// types alise
+typealias Text = String
+typealias Number = Int
+typealias NetWorkResult = (Boolean, String, String, String) -> Unit
+typealias Person = List<String>
+typealias RequestApi = (NetWorkResult)
+
 fun main(){
 
+    val text: Text = "Welcome to Types Alise"
+    val age: Number = 19
+
+    // variable of list
+    val people: Person = listOf("A", "B", "C")
+
+    requestApi { status, message, errorCode, errorMessage -> Unit }
+    requestApi (
+        onResult = { status, message, errorCode, errorMessage ->  Unit
+            println(status)
+            println(message)
+            println(errorCode)
+            println(errorMessage)
+        }
+    )
+
+}
+fun requestApi(
+    onResult: NetWorkResult,
+)
+{
+    // starting requested to the server
+    onResult(
+        true,
+        "System Successfully",
+        "Error Code ",
+        "Messages Code",
+    )
 }
