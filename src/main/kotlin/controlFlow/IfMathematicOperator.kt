881c3c37077dda43de77a1258ecf348e8e7f415c
Nona Yorn <yornnonakh@gmail.com>
2026-05-22 16:30:53 +0700

    add the new feature login

--- src/main/kotlin/controlFlow/IfMathematicOperator.kt
+++ src/main/kotlin/controlFlow/IfMathematicOperator.kt
@@ -2,25 +2,53 @@ package main.kotlin.controlFlow
 
 /*
     Run if with mathematical Operator
+    - Plus (+)
+    - Subtract (-)
+    - Multiply (*)
+    - Divide (/)
+    - Reminder (%)
 
  */
 
 fun main(){
-    // create variable
+
     val myMoney = 500.00
     val friendMoney = 100.00
 
-    // create object
-    val result = compareAndSum(myMoney = myMoney, friendMoney = friendMoney)
-    println("The Result is : $result")
+    val resultOfPlus = comparePlus(myMoney, friendMoney)
+    val resultOfMultiply = compareMultiply(myMoney, friendMoney)
+    val resultOfDivider = compareDivider(myMoney, friendMoney)
+    val resultOfReminder = compareReminder(myMoney,  friendMoney)
+
+    println("Result Of Multiply : $resultOfMultiply")
+    println("Result Of Divider : $resultOfDivider")
+    println("Result Of Reminder is : $resultOfReminder")
+    println("Result Of Plus is : $resultOfPlus")
+
+
+    val resultOfSum = compareAndSum(myMoney = myMoney, friendMoney = friendMoney)
+    println("The Result of Sum is : $resultOfSum")
 }
 
 fun compareAndSum(myMoney: Double,friendMoney: Double): Double{
-    if (myMoney > friendMoney){
-        return myMoney - friendMoney
+    return if (myMoney > friendMoney){
+        myMoney - friendMoney
     }else if (myMoney < friendMoney){
-        return friendMoney - myMoney
+        friendMoney - myMoney
     }else{
-        return myMoney + friendMoney
+        myMoney + friendMoney
     }
+}
+
+fun comparePlus(x: Double, y: Double): Double{
+    return x + y
+}
+fun compareMultiply(x: Double, y: Double): Double{
+    return x * y
+}
+fun compareDivider(a: Double, b: Double): Double{
+    return a / b
+}
+fun compareReminder(x: Double, y: Double): Double{
+    return x % y
 }
