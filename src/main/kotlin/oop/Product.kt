0543158924b8a0a3b7fb4f0e684ca195f0a3fb21
Nona Yorn <yornnonakh@gmail.com>
2026-05-23 20:30:33 +0700

    add the new feature

---
+++ src/main/kotlin/oop/Product.kt
@@ -0,0 +1,11 @@
+package main.kotlin.oop
+
+ /*
+    create product of this store
+  */
+
+class Product {
+    // create properties
+    val dell:  String = "Dell"
+    val asus: String = "Asus"
+}
