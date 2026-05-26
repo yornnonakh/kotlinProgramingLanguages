deba1e99ad846c8f7a67b5574b66d717fc2f2dc3
Nona Yorn <yornnonakh@gmail.com>
2026-05-26 19:32:36 +0700

    update new code

--- src/main/kotlin/oop/Product.kt
+++ src/main/kotlin/oop/Product.kt
@@ -3,9 +3,41 @@ package main.kotlin.oop
  /*
     create product of this store
   */
+ fun main(){
+
+     // create objects
+     val showedCase = ProductShowedCase(
+         "Shose",
+         "Nike",
+         345,
+         343,
+         200.99
+     )
+     println("Showed All The products : $showedCase")
+ }
 
 class Product {
     // create properties
     val dell:  String = "Dell"
     val asus: String = "Asus"
+}
+
+class ProductShowedCase(
+    val name: String,
+    val model: String,
+    val code: Int,
+    val id: Int,
+    val price: Double,
+)
+{
+    // create method
+    fun showedAllTheProductShowedCase(){
+        println("============[Showed All The Products Display ]==============")
+        println("Name : $name")
+        println("Model : $model")
+        println("Code : $code")
+        println("ID : $id")
+        println("Price : $price")
+        println("==============================================================")
+    }
 }
